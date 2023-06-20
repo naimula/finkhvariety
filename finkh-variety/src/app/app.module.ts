40a1a54916e5df61638bf20756cab531aa442b35
Naimul bappy <naimul.atm@gmail.com>
2023-06-20 13:55:13 -0400

    updated

--- finkh-variety/src/app/app.module.ts
+++ finkh-variety/src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { OrderComponent } from './components/order/order.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+ 
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
